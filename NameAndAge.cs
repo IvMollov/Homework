6e719645f3ac29dd668aa38f2d720999577f9bc8
IvMollov <mollov.i@abv.bg>
2016-11-18 22:41:51 +0200

    Add files via upload

---
+++ NameAndAge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameAndAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            int newAge = age + 10;
+            Console.WriteLine("My name is {0}. I'm {1} years old and after 10 years i will be {2} years old.", name, age, newAge);
+        }
+    }
+}
